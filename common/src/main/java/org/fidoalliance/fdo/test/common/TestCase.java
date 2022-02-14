ff418be59bdcf8ae5590c1cbb4846e79904c5a18
Davis Benny <davis.benny@intel.com>
2022-02-14 13:00:20 +0530

    Updating waitTime for docker services
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- common/src/main/java/org/fidoalliance/fdo/test/common/TestCase.java
+++ common/src/main/java/org/fidoalliance/fdo/test/common/TestCase.java
@@ -75,7 +75,7 @@ public abstract class TestCase {
   protected Duration dockerDownTimeout = Duration.of(20, ChronoUnit.SECONDS);
 
   // How long to wait for FIDO Docker services to start up
-  protected Duration fdoDockerUpTimeout = Duration.of(60, ChronoUnit.SECONDS);
+  protected Duration fdoDockerUpTimeout = Duration.of(360, ChronoUnit.SECONDS);
 
   // How long to wait for PRI-FIDO T00
   protected Duration fdoToWait = Duration.of(45, ChronoUnit.SECONDS);
